23d19db1a6e69ab900b75774d7f0bf810d48b2c4
avalojandro <avalojandro@gmail.com>
2019-05-23 20:56:05 -0600

    se subio to do list

---
+++ Javascript/script.js
@@ -0,0 +1,101 @@
+
+function x(nu){
+if (nu == "1")
+{
+  document.getElementById('t1').value = "";
+  document.getElementById('t1').style.background =  "#81F79F";
+}
+if (nu == "2")
+{
+  document.getElementById('t2').value = "";
+  document.getElementById('t2').style.background =  "#81F79F";
+}
+if (nu == "3")
+{
+  document.getElementById('t3').value = "";
+  document.getElementById('t3').style.background =  "#81F79F";
+}
+if (nu == "4")
+{
+  document.getElementById('t4').value = "";
+  document.getElementById('t4').style.background =  "#81F79F";
+}
+if (nu == "5")
+{
+  document.getElementById('t5').value = "";
+  document.getElementById('t5').style.background =  "#81F79F";
+}
+if (nu == "6")
+{
+  document.getElementById('t6').value = "";
+  document.getElementById('t6').style.background =  "#81F79F";
+}
+if (nu == "7")
+{
+  document.getElementById('t7').value = "";
+  document.getElementById('t7').style.background =  "#81F79F";
+}
+if (nu == "8")
+{
+  document.getElementById('t8').value = "";
+  document.getElementById('t8').style.background =  "#81F79F";
+}
+if (nu == "9")
+{
+  document.getElementById('t9').value = "";
+  document.getElementById('t9').style.background =  "#81F79F";
+}
+if (nu == "10")
+{
+  document.getElementById('t10').value = "";
+document.getElementById('t10').style.background =  "#81F79F";
+}
+
+
+}
+
+function f(num){
+
+  if (num == "1")
+  {
+    document.getElementById('t1').style.background = "#2EFE2E";
+  }
+  if (num == "2")
+  {
+    document.getElementById('t2').style.background = "#2EFE2E";
+  }
+  if (num == "3")
+  {
+    document.getElementById('t3').style.background = "#2EFE2E";
+  }
+  if (num == "4")
+  {
+    document.getElementById('t4').style.background = "#2EFE2E";
+  }
+  if (num == "5")
+  {
+    document.getElementById('t5').style.background = "#2EFE2E";
+  }
+  if (num == "6")
+  {
+    document.getElementById('t6').style.background = "#2EFE2E";
+  }
+  if (num == "7")
+  {
+    document.getElementById('t7').style.background = "#2EFE2E";
+  }
+  if (num == "8")
+  {
+    document.getElementById('t8').style.background = "#2EFE2E";
+  }
+  if (num == "9")
+  {
+    document.getElementById('t9').style.background = "#2EFE2E";
+  }
+  if (num == "10")
+  {
+    document.getElementById('t10').style.background = "#2EFE2E";
+  }
+
+
+}
